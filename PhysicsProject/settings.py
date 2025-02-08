fb1de17590d33aea381e77773afe0fc58447e80b
Yasser <yasernet72@gmail.com>
2025-02-08 21:54:59 +0330

    Added virtual environment and updated .gitignore

--- PhysicsProject/settings.py
+++ PhysicsProject/settings.py
@@ -11,24 +11,20 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# Load environment variables from .env file
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', 'yes', '1']
 
 ALLOWED_HOSTS = []
 
